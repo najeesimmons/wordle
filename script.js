62b8812a56ea7f409998dcfbad64eb8f0ca6e3f0
najeesimmons <najee.bryant.simmons@gmail.com>
2022-04-01 16:42:28 -0400

    working onstop clause for insertkey when characters entered are at 5

--- script.js
+++ script.js
@@ -62,10 +62,38 @@ function insertKey(e) {
         console.log("Keep guessing letters...")
     }
     // What do I need to add here to stop insertKey if currentwordGuessArray are at a multiple of 5 but have not been sumbitted.?
-    if (e.key != "Enter" && counter % 5 === 0) {
-        console.log("This condition is met.")
-        return
-    } 
+    if (e.key != "Enter" && counter > 0 && counter % 5 === 0) {
+        if (guessCounter === 0) {
+            if (wordGuessArray0.length % 5 == 0)
+            console.log("Press ENTER to submit yuor guess")
+            return
+        }
+        if (guessCounter === 1) {
+            if (wordGuessArray1.length % 5 == 0)
+            console.log("Press ENTER to submit yuor guess")
+            return
+        }
+        if (guessCounter === 2) {
+            if (wordGuessArray2.length % 5 == 0)
+            console.log("Press ENTER to submit yuor guess")
+            return
+        }
+        if (guessCounter === 3) {
+            if (wordGuessArray3.length % 5 == 0)
+            console.log("Press ENTER to submit yuor guess")
+            return
+        }
+        if (guessCounter === 4) {
+            if (wordGuessArray4.length % 5 == 0)
+            console.log("Press ENTER to submit yuor guess")
+            return
+        }
+        if (guessCounter === 5) {
+            if (wordGuessArray5.length % 5 == 0)
+            console.log("Press ENTER to submit yuor guess")
+            return
+        }
+    }       
     if (e.key != "Enter") {  
         counter += 1
         
