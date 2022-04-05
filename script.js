654c7a9cc14aa1c719121b7318cfe0620b6da3dc
najeesimmons <najee.bryant.simmons@gmail.com>
2022-04-05 14:55:04 -0400

    improving refactor

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // this array contains words to be used as answers
-const wordsArray = ["movie", "eagle", "scare", "renew", "prime", "slime"]
+const wordsArray = ["movie", "scare", "clear", "crime", "slime", "clash"]
 let randomWord = null
 let wordGuess = null
 let counter = 0
@@ -11,7 +11,6 @@ let guesses = new Array(rows); // create an empty array of "rows"
 for (let i = 0; i < rows; i++) {
     guesses[i] = new Array(0); // make each element an array
   }
-  console.log(guesses);
 
 // this function will initate the game by choosing a word
 wordSelector()
@@ -29,12 +28,14 @@ window.addEventListener('keydown', insertKey)
 // this function takes the pressed character and puts its text into the current div
 function insertKey(e) {
     //insertKey also needs to handle pushing entry into the nested array for the current guess
-    if (e.key === "Enter" && counter % 5 === 0 && counter > 0) {
+    if (e.key === "Enter" && counter % 5 === 0 && guesses[guessCounter].length === 5) {
         console.log("Next, we will run checkAnswer.")
         checkAnswer(guesses[guessCounter])
         guessCounter += 1
+        return
     }
-    if (e.key === "Enter" && counter % 5 != 0) {
+    if (e.key === "Enter" && guesses[guessCounter].length != 5) {
+        console.log(guesses[guessCounter] , guesses[guessCounter].length)
         alert("A guess MUST consist of 5 letters -- no more, no less!")
     } 
     if (e.key === "Backspace") {
@@ -78,10 +79,9 @@ function checkAnswer() {
                 let styleThisBox = document.getElementById(`box${i}`)
                 styleThisBox.classList.add("right-guess")
             }
-        alert("You guessed the correct word!")
+        alert("You guessed the correct word! Refresh page to play again!")
         console.log("You win!")
         window.removeEventListener('keydown', insertKey)
-        alert("Refresh page to play again!")
         return
     } else {
         let allBoxes = document.querySelectorAll('.box')
