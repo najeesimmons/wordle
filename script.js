22c25a1222fe630334c79f4dcad35980cda25b2e
najeesimmons <najee.bryant.simmons@gmail.com>
2022-04-05 13:48:17 -0400

    refactoring working so far

--- script.js
+++ script.js
@@ -4,12 +4,13 @@ let randomWord = null
 let wordGuess = null
 let counter = 0
 let guessCounter = 0
-let wordGuessArray0 = []
-let wordGuessArray1 = []
-let wordGuessArray2 = []
-let wordGuessArray3 = []
-let wordGuessArray4 = []
-let wordGuessArray5 = []
+
+let rows = 6
+let guesses = new Array(rows); // create an empty array of "rows"
+for (let i = 0; i < rows; i++) {
+    guesses[i] = new Array(0); // make each element an array
+  }
+  console.log(guesses); //  Output: [ [ <5 empty items> ], [ <5 empty items> ], [ <5 empty items> ], [ <5 empty items> ] ]
 
 // this function will initate the game by choosing a word
 wordSelector()
@@ -29,7 +30,7 @@ function insertKey(e) {
     //insertKey also needs to handle pushing entry into wordGuessArray
     if (e.key === "Enter" && counter % 5 === 0) {
         console.log("Next, we will run checkAnswer.")
-        checkAnswer()
+        checkAnswer(guesses[guessCounter])
         guessCounter += 1
     }
     if (e.key === "Enter" && counter % 5 != 0) {
@@ -39,24 +40,7 @@ function insertKey(e) {
         console.log(counter)
         let currentBox = document.getElementById(`box${counter}`)
         currentBox.innerText=""
-        if (guessCounter === 0) {
-            wordGuessArray0.pop()
-        }
-        if (guessCounter === 1) {
-            wordGuessArray1.pop()
-        }
-        if (guessCounter === 2) {
-            wordGuessArray2.pop()
-        }
-        if (guessCounter === 3) {
-            wordGuessArray3.pop()
-        }
-        if (guessCounter === 4) {
-            wordGuessArray4.pop()
-        }
-        if (guessCounter === 5) {
-            wordGuessArray5.pop()
-        }
+        guesses[guessCounter].pop()
         counter -=1
         console.log(counter)
         return   
@@ -64,39 +48,7 @@ function insertKey(e) {
     if (e.key != "Enter" && counter % 5 != 0) {
         console.log("Keep guessing letters...")
     }
-    // What do I need to add here to stop insertKey if currentwordGuessArray are at a multiple of 5 but have not been sumbitted.?
-    // if (e.key != "Enter" && counter > 0 && counter % 5 === 0) {
-    //     if (guessCounter === 0) {
-    //         if (wordGuessArray0.length % 5 == 0)
-    //         console.log("Press ENTER to submit your guess")
-    //         return
-    //     }
-    //     if (guessCounter === 1) {
-    //         if (wordGuessArray1.length % 5 == 0)
-    //         console.log("Press ENTER to submit your guess")
-    //         return
-    //     }
-    //     if (guessCounter === 2) {
-    //         if (wordGuessArray2.length % 5 == 0)
-    //         console.log("Press ENTER to submit your guess")
-    //         return
-    //     }
-    //     if (guessCounter === 3) {
-    //         if (wordGuessArray3.length % 5 == 0)
-    //         console.log("Press ENTER to submit your guess")
-    //         return
-    //     }
-    //     if (guessCounter === 4) {
-    //         if (wordGuessArray4.length % 5 == 0)
-    //         console.log("Press ENTER to submit your guess")
-    //         return
-    //     }
-    //     if (guessCounter === 5) {
-    //         if (wordGuessArray5.length % 5 == 0)
-    //         console.log("Press ENTER to submit your guess")
-    //         return
-    //     }
-    // }       
+    
     if (e.key != "Enter") {  
         counter += 1
         
@@ -107,30 +59,9 @@ function insertKey(e) {
         if (counter >= "31") {
             window.removeEventListener('keydown', insertKey) 
             return
-        }
-        if (guessCounter === 0) {
-            wordGuessArray0.push(`${e.key}`)
-            console.log(`Your guess so far consists of "${wordGuessArray0}"`)
-        }
-        if (guessCounter === 1) {
-            wordGuessArray1.push(`${e.key}`)
-            console.log(`Your guess so far consists of "${wordGuessArray1}"`)
-        }
-        if (guessCounter === 2) {
-            wordGuessArray2.push(`${e.key}`)
-            console.log(`Your guess so far consists of "${wordGuessArray2}"`)
-        }
-        if (guessCounter === 3) {
-            wordGuessArray3.push(`${e.key}`)
-            console.log(`Your guess so far consists of "${wordGuessArray3}"`)
-        }
-        if (guessCounter === 4) {
-            wordGuessArray4.push(`${e.key}`)
-            console.log(`Your guess so far consists of "${wordGuessArray4}"`)
-        }
-        if (guessCounter === 5) {
-            wordGuessArray5.push(`${e.key}`)
-            console.log(`Your guess so far consists of "${wordGuessArray5}"`)
+        } else {
+            guesses[guessCounter].push(`${e.key}`)
+            console.log(`Your guess so far consists of "${guesses[guessCounter]}"`)
         }
         currentBox.innerText=`${e.key}`
         console.log(counter)
@@ -143,9 +74,8 @@ function insertKey(e) {
 // it will check each letter and compare against randomWord
 function checkAnswer() {
     let randomWordArray = randomWord.split("") 
-    const guessWordArray = getWordGuessArray()
-    console.log({guessWordArray, randomWordArray, guessCounter})
-    if (JSON.stringify(guessWordArray) === JSON.stringify(randomWordArray)) {
+    console.log(guesses[guessCounter], randomWordArray, guessCounter)
+    if (JSON.stringify(guesses[guessCounter]) === JSON.stringify(randomWordArray)) {
         if (counter === 5) {
             for (let i = 1; i < counter+1; i++) {
                 let styleThisBox = document.getElementById(`box${i}`)
@@ -193,8 +123,8 @@ function checkAnswer() {
         const boxes = getBoxRow(allBoxesForStyles)
         console.log(boxes)
         for (let i = 0; i < randomWordArray.length; i++) {
-            if (randomWordArray[i] !== guessWordArray[i]) {
-                let testedLetter = guessWordArray[i]
+            if (randomWordArray[i] !== guesses[guessCounter][i]) {
+                let testedLetter = guesses[guessCounter][i]
                 console.log(`The letter ${testedLetter} is not found at this position. Now testing to see if it's in this word at all...`)
                 // test letter by looping through random word to see if it appears elsewhere
                 for (let j = 0; j < randomWordArray.length; j++) {
@@ -214,36 +144,14 @@ function checkAnswer() {
                     }
                 }
             } else {
-                console.log(`"${guessWordArray[i]}" is the correct letter, in the correct place.`)
+                console.log(`"${guesses[guessCounter][i]}" is the correct letter, in the correct place.`)
                 console.log(`the coutner is at ${counter}`)
                 let styleNow = boxes[i]
                 styleNow.classList.add('right-guess')
             } 
         }
-        console.log(`Your next guess now consists of "${guessWordArray}"`)
+        console.log(`Your next guess now consists of "${guesses[guessCounter]}"`)
         }     
-}
-
-
-function getWordGuessArray () {
-    if (guessCounter === 0) {
-        return wordGuessArray0
-    }
-    if (guessCounter === 1) {
-        return wordGuessArray1
-    }
-    if (guessCounter === 2) {
-        return wordGuessArray2
-    }
-    if (guessCounter === 3) {
-        return wordGuessArray3
-    }
-    if (guessCounter === 4) {
-        return wordGuessArray4
-    }
-    if (guessCounter === 5) {
-        return wordGuessArray5
-    }
 }
 
 function getBoxRow (boxes) {
